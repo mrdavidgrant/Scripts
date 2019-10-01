60332373447d641fdb79a4767eccdd7a981b0da9
David Grant <mrdavidgrant@outlook.com>
2019-10-01 10:06:57 -0400

    deleted public repository

--- import script 2.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  var gr = new GlideRecord ('u_facilities_ccohs');
-  gr.addNullQuery('u_role');
-  gr.query();
-  while (gr.next()){
-    gr.u_status = 'No Longer Required';
-    gr.update();
-  }
-})()
--- stuff.js
+++
@@ -1,5 +0,0 @@
-var rec = new GlideRecord('sys_number');
-rec.get('973c8e8a9d022000da615b13b3a22f32');
-//Push the record into the current update set   
-var um = new GlideUpdateManager2();
-um.saveRecord(rec);
